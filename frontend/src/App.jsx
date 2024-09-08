4906013127eadc606c3c7dcb25732265c20fdfcd
Bavirisetty Hemanth Manikanta <72919457+hemanth-bavirisetty@users.noreply.github.com>
2024-09-08 15:57:47 +0530

    Cors polocy resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,33 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios';
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [jokes,setJokes] = useState([])
+  const [jokes, setJokes] = useState([]);
 
-  useEffect(()=>{
-    axios.get('/api/jokes')
-    .then((res)=>{
-      setJokes(res.data)
-      console.log(res.data)})
-  },[])
+  useEffect(() => {
+    axios.get("/api/jokes").then((res) => {
+      setJokes(res.data);
+      console.log(res.data);
+    });
+  }, []);
 
   return (
-   <>
-   <h1>frontend Project</h1>
-   <h2>jokes:{jokes.length}</h2>
 
-   {
-    jokes.map((joke)=>{
-      return <div key={joke.id}>
-        <h1>{joke.title}</h1>
-        <h2>{joke.content}</h2>
-      </div>
-    })
-   }
-   </>
-  )
+    <>
+      <h1>frontend Project</h1>
+      <h2>jokes:{jokes.length}</h2>
+
+      {jokes.map((joke) => {
+        return (
+          <div key={joke.id}>
+            <h1>{joke.title}</h1>
+            <h2>{joke.content}</h2>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
-export default App
+export default App;
